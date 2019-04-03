e8e65d1f164e4f94626d3d0f4b0d673188146fdc
hugchange <285893374@qq.com>
2019-04-03 17:06:23 +0800

    vue 项目环境搭建

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div id="app" class="g-container">
+    <div class="g-view-container">
+      <router-view/>
+    </div>
+    <div class="g-footer-container">
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'App',
+    components: {}
+  };
+</script>
+
+<style>
+</style>
---
+++ src/components/index.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    运行环境搭建成功 ^_^
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'index'
+  };
+</script>
+
+<style scoped>
+
+</style>
